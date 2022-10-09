bd25311ab01ec0b6b4188f136498accf39b511e1
Lê Quang Thắng <lthang052@gmail.com>
2022-10-09 19:08:40 +0700

    initialize repository

---
+++ RauCuXanh/RauCuXanh/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RauCuXanh
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
